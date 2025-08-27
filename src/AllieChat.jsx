7c7b187c1961be3522cb8f61dea87539e01ee0ee
vin2206 <factonomy01@gmail.com>
2025-08-28 03:39:27 +0530

    Update AllieChat.jsx

--- src/AllieChat.jsx
+++ src/AllieChat.jsx
@@ -865,6 +865,7 @@ if (!user) {
 )}
 
       <div className="chat-container">
+        <div className="chat-spacer"></div>
         {displayedMessages.map((msg, index) => (
           <div key={index} className={`message ${msg.sender === 'user' ? 'user-message' : 'allie-message'}`}>
   <span className={`bubble-content ${msg.audioUrl ? 'has-audio' : ''}`}>
@@ -895,7 +896,6 @@ if (!user) {
     <span></span>
   </div>
 )}
-        <div className="chat-spacer"></div>
         <div ref={bottomRef}></div>
       </div>
 
